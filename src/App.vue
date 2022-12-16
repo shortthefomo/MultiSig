802de03e38c67247814772b2186dac3b9c103653
lathan britz <lathan.britz@drupalconnect.com>
2022-12-16 16:35:59 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,227 @@
+<template>
+    <header class="container">
+        <Landing v-if="components.Landing" @action="buttonAction" :client="client" :Sdk="Sdk" :nodetype="nodetype">{NFT Flush}</Landing>
+    </header>
+
+    <main class="container flex-shrink-0 mb-4">
+        <router-view />
+    </main>
+
+    <footer class="container footer mb-2 mt-auto">
+        <div class="border-top py-3">
+            &copy; Three
+            <Refs />
+        </div>
+    </footer>
+</template>
+
+<script>
+    import Refs from './components/Refs.vue'
+    import Landing from './components/Landing.vue'
+    //import xapp from './plugins/xapp.js'
+    import { XrplClient } from 'xrpl-client'
+
+    const xapp = window.xAppSdk
+
+    import {XummSdkJwt} from 'xumm-sdk'
+
+    export default {
+        components: {
+            Landing,
+            Refs
+        },
+        data() {
+            return {
+                Sdk: new XummSdkJwt(import.meta.env.VITE_APP_KEY),
+                nodetype: 'TESTNET',
+                socket: null,
+                timeout_socket: null,
+                reconnect_socket: 0,
+                pong: false,
+                ready: false,
+                components: {
+                    Landing: false
+                },
+                client: null,
+                signedIn: false
+            }
+        },
+        async mounted() {
+            if (this.components.Landing) { return }
+            await this.jwtFlow()
+            this.components.Landing = true
+            
+        },
+        methods: {
+
+            async getStoreage() {
+			    const storageGet = await this.Sdk.storage.get()
+			    console.log('storageGet', storageGet)
+            },
+            async jwtFlow() {
+                const tokenData = await this.Sdk.getOttData()
+                console.log('tokenData', tokenData)
+                this.$store.dispatch('xummTokenData', tokenData)
+                console.log('account', tokenData.account)
+                this.$store.dispatch('setAccount', tokenData.account)
+                this.nodetype = tokenData.nodetype
+
+                const servers = [tokenData.nodewss]
+                if (tokenData.nodetype == 'MAINNET') {
+                    servers.unshift('wss://node.panicbot.xyz')
+                }
+                console.log('ws servers', servers)
+                
+                this.client = new XrplClient(servers)
+
+                const callback = async (event) => {
+                    let request = {
+                        'id': 'xrpl-local',
+                        'command': 'ledger',
+                        'ledger_hash': event.ledger_hash,
+                        'ledger_index': 'validated',
+                        'transactions': true,
+                        'expand': true,
+                        'owner_funds': true
+                    }
+    
+                    const ledger_result = await this.client.send(request)
+                    if ('error' in ledger_result) {
+                        console.log('XRPL error', ledger_result)
+                    }
+                    
+                    if ('ledger' in ledger_result) {
+                        // console.log('ledger', ledger_result)
+                        this.$store.dispatch('setLedger', ledger_result.ledger.ledger_index)
+                    }
+                }
+                this.client.on('ledger', callback)
+                await this.jwtSignIn()
+            },
+            async jwtSignIn() {
+                const self = this
+                const request  = { txjson: { TransactionType: 'SignIn' }}
+                // const subscription = await this.Sdk.payload.create(request)
+
+                const subscription = await this.Sdk.payload.createAndSubscribe(request, async event => {
+                    console.log('New payload event:', event.data)
+
+                    if (event.data.signed === true) {
+                        console.log('Woohoo! The sign request was signed :)')
+                        self.signedIn = true
+                        self.$store.dispatch('setUserToken', event.data.payload_uuidv4)
+                        await self.connectWebsocket()
+                        await self.getStoreage()
+                        return event.data
+                    }
+
+                    if (event.data.signed === false) {
+                        console.log('The sign request was rejected :(')
+                        xapp.close({ refreshEvents: true })
+                            .then(d => {
+                                // d (returned value) can be Error or return data:
+                                console.log('close response:', d instanceof Error ? d.message : d)
+                            })
+                            .catch(e => console.log('Error:', e.message))
+                        return false
+                    }
+                })
+                console.log('subscription', subscription)
+
+                xapp.openSignRequest({ uuid: subscription.created.uuid })
+                    .then(d => {
+                        // d (returned value) can be Error or return data:
+                        console.log('openSignRequest response:', d instanceof Error ? d.message : d)
+                    })
+                    .catch(e => console.log('Error:', e.message))
+            },
+            async connectWebsocket() {
+                const self = this
+                console.log('location', window.location.origin)
+                if ('https://192.168.0.20:3007' == window.location.origin) {
+                    console.log('using local socket')
+                    this.socket = new WebSocket(this.connection.socket_local)
+                }
+                else {
+                    console.log('using remote socket')
+                    this.socket = new WebSocket(this.connection.socket)    
+                }
+                const server_info = await this.client.getState()
+                console.log('client connection', server_info)
+
+                this.reconnect_socket++
+                this.socket.onopen = function (message) {
+                    const tokenData = self.$store.getters.getXummTokenData
+                    self.socket.send(JSON.stringify({
+                        request: 'SUBSCRIBE',
+                        message: {
+                            app: 'MultiSig',
+                            appkey: import.meta.env.VITE_APP_KEY,
+                            account: self.$store.getters.getAccount, 
+                            uuid: self.$store.getters.getUserToken, 
+                            version: tokenData.version,
+                            locale: tokenData.locale,
+                            currency: tokenData.currency,
+                            nodetype: tokenData.nodetype,
+                            nodewss: server_info.server.uri,
+                            user: tokenData.user
+                        },
+                        channel: self.$store.getters.getAccount
+                    }))
+                    self.ping()
+
+                    console.log('NFT Flush sockets connected! :)')
+                }
+                this.socket.onmessage = async function (message) {
+                    if (self.timeout_socket != null) {
+                        clearTimeout(self.timeout_socket)
+                        self.timeout_socket = null
+                    }
+                    let data = JSON.parse(message.data)
+                    const account = self.$store.getters.getAccount
+                    if (account in data) {
+                        if ('PONG' in data[account]) {
+                            self.pong = true
+                        }
+                        if ('SUBSCRIBED' in data[account]) {
+                            console.log('SUBSCRIBED!')
+                        }
+                    }
+                }
+                setInterval(() => {
+                    self.pong = false
+                    self.ping()
+                }, 10_000)
+
+                this.socket.onerror = function (message) {
+                    console.log('There was an error connection to three MultiSig socket! :(')
+                    console.log(message)
+                    self.socket.close()
+                }
+
+                this.socket.onclose = function (message) {
+                    console.log('three socket MultiSig disconnected!', message)
+                    if (self.timeout_socket == null && message.code != 1005) {
+                        self.timeout_socket = setTimeout(async () => {
+                            if (self.reconnect_socket < 30) {
+                                await self.connectWebsocket() 
+                            }
+                        }, 3000)
+                    }
+                }
+            },
+            ping() {
+                if (this.$store.getters.getAccount != '') {
+                    this.socket.send(JSON.stringify({
+                        request: 'PING',
+                        message: {account: this.$store.getters.getAccount},
+                        channel: this.$store.getters.getAccount
+                    }))
+                }
+                else {
+                    console.log('account is empty')
+                }
+            },
+        }
+    }
+</script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <img class="bog-roll" src="/e.png">
+            <p class="col-md-8 fs-4">
+                <p class="text-muted text-end fs-6">by three</p>
+            </p>
+            <p class="col-md-12 fs-4">
+                <p class="text-center">
+                    <p>ToDO</p>
+                </p>
+            </p>
+        </div>
+    </div>
+
+    <footer>
+        <p>XRPL ledger: {{ledger}}</p>
+        <p>account: {{account}}</p>
+    </footer>
+</template>
+
+<script>
+    const xapp = window.xAppSdk
+    import { Buffer } from 'buffer'
+
+    export default {
+        name: 'Landing',
+        props: ['client', 'Sdk', 'nodetype'],
+        data() {
+            return {
+                isLoading: true,
+                selectedRows: [],
+                ascending: false
+            }
+        },
+        async mounted() {
+            console.log('landing mounted...')
+            
+        },
+        computed: {
+            
+        },
+        watch: {
+            async account() {
+                if (this.$store.getters.getAccount != '') {
+                    console.log('account changed', this.account)
+                }
+            }
+        },
+        methods: {
+            sortTable(col) {
+                if (this.sortColumn === col) {
+                    this.ascending = !this.ascending
+                } else {
+                    this.ascending = true
+                    this.sortColumn = col
+                }
+
+                let ascending = this.ascending
+
+                this.NFTokenOffers.sort(function(a, b) {
+                    if (a[col] > b[col]) {
+                    return ascending ? 1 : -1
+                    } else if (a[col] < b[col]) {
+                    return ascending ? -1 : 1
+                    }
+                    return 0
+                })
+            },
+            currencyHexToUTF8(code) {
+				if (code.length === 3)
+					return code
+
+				let decoded = new TextDecoder()
+					.decode(this.hexToBytes(code))
+				let padNull = decoded.length
+
+				while (decoded.charAt(padNull - 1) === '\0')
+					padNull--
+
+				return decoded.slice(0, padNull)
+			},
+            hexToBytes(hex) {
+				let bytes = new Uint8Array(hex.length / 2)
+
+				for (let i = 0; i !== bytes.length; i++) {
+					bytes[i] = parseInt(hex.substr(i * 2, 2), 16)
+				}
+
+				return bytes
+			}
+        },
+    }
+</script>
+<style scoped>
+    .bog-roll {
+        width: 100%;
+    }
+    .main-text {
+        color: #753ee2;
+        -webkit-text-stroke-width: 1px;
+        -webkit-text-stroke-color: black;
+    }
+    .btn-purple {
+        background-color: #753ee2;
+        color: #ffffff;
+    }
+
+    .btn-yellow {
+        background-color: #ffc107;
+        color: #ffffff;
+    }
+    .btn-pink {
+        background-color: #ff1a8b;
+        color: #ffffff;
+    }
+
+    .btn-green {
+        background-color: #00e56a;
+        color: #ffffff;
+    }
+</style>
