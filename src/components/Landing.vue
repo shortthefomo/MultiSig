542f9d8582dfa4bedcbe294af5f3aa388a376192
lathan britz <lathan.britz@drupalconnect.com>
2022-12-18 12:45:40 -0300

    dev

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -68,7 +68,7 @@
                     payload.marker = marker
                 }
                 let res = await this.client.send(payload)
-)
+
                 for (let index = 0; index < res.account_objects.length; index++) {
                     const element = account_objects[index]
                     if (element.LedgerEntryType === 'SignerList') {
