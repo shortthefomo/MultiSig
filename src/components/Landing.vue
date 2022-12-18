4a32a5e65d729cc381ea342671b1b517855d7856
lathan britz <lathan.britz@drupalconnect.com>
2022-12-18 20:02:46 -0300

    dev

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-2 mb-2 bg-light rounded-3">
         <div class="container-fluid py-3">
-            <p class="h1 text-center">{{ledger}}</p>
+            <p class="h1 text-start">{{ledger}}</p>
             <p class="p-2 mb-2 bg-warning"><small>{{account}}</small></p>
             <div class="col-md-8 fs-4">                
                 <p class="text-muted text-end fs-6"><span class="fancy-font">multisign</span>  by three</p>
