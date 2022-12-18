dad60308eff34384d17bd5447ddaef4576d7ccaf
lathan britz <lathan.britz@drupalconnect.com>
2022-12-18 18:01:50 -0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
     </main>
 
     <footer class="container bg-dark footer position-absolute bottom-0 start-50 translate-middle-x text-center">
+        <span class="text-light fancy-font text-start ms-2 mb-4">scan qr code -> </span>
         <button @click="openSign" class="btn btn-default mt-2 mb-4" role="button" id="open-sign">
-            <span class="text-light fancy-font text-left ms-2 mb-4">scan qr code -> </span><img src="/apple-touch-icon.png" class="p-1 border border-1 rounded-3" alt="open sign" width="60" />
+            <img src="/apple-touch-icon.png" class="p-1 border border-1 rounded-3" alt="open sign" width="60" />
         </button>
     </footer>
 </template>
