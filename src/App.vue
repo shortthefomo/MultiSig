62d2e245d4c0c269483122adc581406d231a4215
lathan britz <lathan.britz@drupalconnect.com>
2022-12-18 12:40:44 -0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="container">
-        <Landing v-if="components.Landing" @action="buttonAction" :client="client" :Sdk="Sdk" :nodetype="nodetype">{MultiSig Landing}</Landing>
+        <Landing v-if="components.Landing" :client="client" :Sdk="Sdk" :nodetype="nodetype">{MultiSig Landing}</Landing>
     </header>
 
     <main class="container flex-shrink-0 mb-4">
