4edef79b9af8f8dec96328f6e52d7267201a8fe4
lathan britz <lathan.britz@drupalconnect.com>
2022-12-19 14:21:43 -0300

    dev

--- src/components/ModalSignerList.vue
+++ src/components/ModalSignerList.vue
@@ -26,7 +26,7 @@
                         </button>
                         <div v-for="(signers, index) in signerList" :key="index">
                             <div class="flex justify-start ms-2 mt-4">
-                                <input :id="index + 'rAddress'" v-model="signers.rAddress" placeholder="r address" class="w-full py-2 border border-indigo-500 rounded" />
+                                <input :id="index + 'rAddress'" v-model="signers.address" placeholder="r address" class="w-full py-2 border border-indigo-500 rounded" />
                                 <!-- <label class="form-check-label" :for="index + 'weight'"> rAddress</label> -->
                                 <input :id="index + 'weight'" v-model="signers.weight" placeholder="weight" class="w-full py-2 border border-indigo-500 rounded" />
                                 <!-- <label class="form-check-label" :for="index + 'weight'"> Weight</label> -->
