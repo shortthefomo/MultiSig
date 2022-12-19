27d42b0bd50c6710a8e2c3bab7cd1a044b289b38
lathan britz <lathan.britz@drupalconnect.com>
2022-12-19 00:03:09 -0300

    dev

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -5,8 +5,8 @@
             <p class="p-2 mb-2 bg-warning">
                 <small class="text-wrap">{{account}}</small>
                 <sup>
-                    <i v-if="masterKeyEnabled && signer_lists.length == 0" class="master-key position-absolute top-0 start-0 h1 bi bi-patch-check-fill text-primary"></i>
-                    <i v-else class="master-key position-absolute top-0 start-0 h1 bi bi-patch-minus-fill text-orange"></i>
+                    <i v-if="masterKeyEnabled && signer_lists.length == 0" class="master-key position-absolute top-0 start-0 h1 bi bi-patch-check-fill text-green"></i>
+                    <i v-else class="master-key position-absolute top-0 start-0 h1 bi bi-patch-minus-fill text-pink"></i>
                 </sup>
             </p>
             
@@ -212,6 +212,14 @@
 
     .text-orange {
         color: #ff8f07;
+    }
+
+    .text-green {
+        color: #00e56a;
+    }
+
+    .text-pink {
+        color: #ff1a8b;
     }
 
     .btn-yellow {
