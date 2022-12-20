852853e15a2a60139ffd9513ff764eec9c2b9e8f
lathan britz <lathan.britz@drupalconnect.com>
2022-12-19 22:45:26 -0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,7 @@
                     xapp.openSignRequest({ 'uuid': data.qrContents.split('/')[4] })
                         .then(d => {
                             // d (returned value) can be Error or return data:
-                            console.log('openSignRequest response:', d instanceof Error ? d.message : d)
+                            // console.log('openSignRequest response:', d instanceof Error ? d.message : d)
                         })
                         .catch(e => console.log('Error:', e.message))
                 })
@@ -74,7 +74,7 @@
                 xapp.scanQr()
                     .then(d => {
                         // d (returned value) can be Error or return data:
-                        console.log('scanQr response:', d instanceof Error ? d.message : d)
+                        // console.log('scanQr response:', d instanceof Error ? d.message : d)
                         if (!(d instanceof Error)) {
                             //console.log('look uuid', d.message.qrContents.split('/'))
                         }
@@ -150,7 +150,7 @@
                         xapp.close({ refreshEvents: true })
                             .then(d => {
                                 // d (returned value) can be Error or return data:
-                                console.log('close response:', d instanceof Error ? d.message : d)
+                                // console.log('close response:', d instanceof Error ? d.message : d)
                             })
                             .catch(e => console.log('Error:', e.message))
                         return false
@@ -161,7 +161,7 @@
                 xapp.openSignRequest({ uuid: subscription.created.uuid })
                     .then(d => {
                         // d (returned value) can be Error or return data:
-                        console.log('openSignRequest response:', d instanceof Error ? d.message : d)
+                        // console.log('openSignRequest response:', d instanceof Error ? d.message : d)
                     })
                     .catch(e => console.log('Error:', e.message))
             },
--- src/components/ModalSignerList.vue
+++ src/components/ModalSignerList.vue
@@ -139,7 +139,7 @@ export default {
         xapp.openSignRequest({ uuid: subscription.created.uuid })
             .then(d => {
                 // d (returned value) can be Error or return data:
-                console.log('openSignRequest response:', d instanceof Error ? d.message : d)
+                //log('openSignRequest response:', d instanceof Error ? d.message : d)
             })
             .catch(e => console.log('Error:', e.message))
         },
