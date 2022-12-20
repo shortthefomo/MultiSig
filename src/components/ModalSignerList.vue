bbf1255721f61570cb4aee90915f0e4285f90288
lathan britz <lathan.britz@drupalconnect.com>
2022-12-20 10:52:13 -0300

    dev

--- src/components/ModalSignerList.vue
+++ src/components/ModalSignerList.vue
@@ -46,7 +46,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-secondary" @click="removeSignerList">Remove Signer List</button>
+                <button v-if="hasSignerList" type="button" class="btn btn-secondary" @click="removeSignerList">Remove Signer List</button>
                 <button type="button" class="btn btn-green" @click="createSignerList">Apply </button>
             </div>
             </div>
