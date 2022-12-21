5b9612eba09ebd0f717e325e3c1c3336ef6910d7
lathan britz <lathan.britz@drupalconnect.com>
2022-12-21 13:59:43 -0300

    dev

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -14,8 +14,8 @@
                 <p class="text-muted text-end fs-6"><span class="fancy-font">siglists</span>  by three</p>
             </div>
             <div class="col-md-8 fs-4">
-                <div :class="regularKeyClass()"><i class="bi bi-list-check"></i></div>
-                <div :class="signerListClass()"><i class="bi bi-key-fill"></i></div>
+                <span :class="regularKeyClass()"><i class="bi bi-list-check"></i></span>
+                <span :class="signerListClass()"><i class="bi bi-key-fill"></i></span>
             </div>
             <p>
                 <a v-if="masterKey && signer_lists.length > 0" class="btn btn-pink mb-2" @click="removeMasterKey" role="button" id="remove-master">Remove Master Key</a>
