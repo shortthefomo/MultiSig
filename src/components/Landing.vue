53381b16508dd5e2bbb4caaa6c7ed45e66f47bff
lathan britz <lathan.britz@drupalconnect.com>
2022-12-22 11:39:02 -0300

    dev

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -13,8 +13,10 @@
                     <i v-else class="master-key position-absolute top-0 start-0 h1 bi bi-patch-minus-fill text-dark"></i>
                 </sup>
             </p>
+            <p class="p-2 mb-2">
+                <small>RegularKey</small>
+            </p>
             <p class="p-2 mb-2 bg-dark text-white">
-                Regular Key<br/>
                 <small class="text-wrap">{{regularKeyAddress}}</small>
             </p>
             <div class="col-md-8 fs-4 mb-5">  
